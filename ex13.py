9cfa2a7ae69f2cf967fabd839272426ed8605fde
Ivan <vanzaj@gmail.com>
2016-05-03 13:41:01 +0800

    Review ex 13

--- ex13.py
+++ ex13.py
@@ -9,11 +9,13 @@ print "Your third variable is:", third
 
 # On line 1 we have a import. This is the thing,
 # we add features to the scipt from Python feature set,
-# That's why we need to key in the feature when we run \n
+# That's why we need to key in the feature when we run
 # this program, and this can keep the program small.
 
+# Is there a difference between Python feature set and Python standard library?
+
 # The argv is "argument variable.'.
-# THIS variable holds the arguments, that can pass the \n
+# THIS variable holds the arguments, that can pass the 
 # Python script when we run it.
 
 # The line 3 is to "unpacks" the argv, not holding it.
@@ -24,3 +26,13 @@ print "Your third variable is:", third
 # This is called "called libraries" module.
 
 # Sys - System specific parameters and functions
+
+
+# try the following:
+
+data = [1, "two", 3.0]
+one, two, three = data
+print "one is %r; two is %r; three is %r" % (one, two, three)
+
+# compare with the "unpacking" line for `argv`
+# what is the type of `argv` and `data`?
