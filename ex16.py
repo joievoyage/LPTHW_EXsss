86d03d0601d5ceb4a7cf09c16e19eb3fc0274e0d
Ivan <vanzaj@gmail.com>
2016-05-03 14:18:52 +0800

    Review ex16

--- ex16.py
+++ ex16.py
@@ -5,12 +5,15 @@ script, filename = argv
 print "We're going to erase %r." % filename
 print "If you don't want that, hit CTRL-C (^C)."
 print "If you do want that, hit RETURN." ## These first few lines is a function
+                                         # In Python 2.x, `print` is a statement, not a function
+                                         # but in Python 3 `print` became function that is why you will have to use ()
+                                         # print("We're going to erase %r." % filename)
 
-raw_input("?")
+raw_input("?")  # note that we are not saving the output of `raw_input` into any variable
 
 print "Opening the file..."
 target = open(filename, 'w') ## Then these 3 lines is another function.
-                             ## File name 'w' is a string character, 'w' means open the file in 'write' mode,
+                             ## 'w' is a 2d argument for `open`. it's a string character. 'w' means open the file in 'write' mode
                              ## We also can use 'r' for read, and 'a' for append.
 print "Truncaitng the file. Goodbye!"
 target.truncate()  ## Empty the file. <<Caution!!>> if need the file, have to use it carefully!
@@ -23,8 +26,8 @@ line3 = raw_input("line 3: ")
 
 print "I'm going to write these to the file."
 
-target.write(line1) ## write(something) means write (something) inside a file.
-target.write("\n")
+target.write(line1) ## write(something) means write the value of `something` into a file variable `target`.
+target.write("\n")  # write "new line" character
 target.write(line2)
 target.write("\n")
 target.write(line3)
@@ -33,3 +36,7 @@ target.write("\n")
 print "And finally, we close it."
 target.close()  ## close the file. Just like File --> Save--> close
 
+
+# try the following in the terminal:
+# cp ex16.py ex16_copy.py
+# python ex16_copy.py ex16_copy.py
